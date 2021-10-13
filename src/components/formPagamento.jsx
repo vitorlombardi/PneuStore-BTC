a0001cd6cc3079849805237057d47b8ada44ab9b
vitorlombardi <83724421+vitorlombardi@users.noreply.github.com>
2021-10-13 17:59:59 -0300

    update formPagamento (create dropdown de parcelamento)

--- src/components/formPagamento.jsx
+++ src/components/formPagamento.jsx
@@ -1,47 +1,67 @@
-import React from 'react'
-import '../styles/formPagamento.scss'
+import React from "react";
+import "../styles/formPagamento.scss";
 
-export default function FormPagamento({setResumo, setPagamento}) {
+export default function FormPagamento({ setResumo, setPagamento }) {
+  const handleClick = () => {
+    setPagamento(false);
+    setResumo(true);
+  };
 
-    const handleClick = () => {
-        setPagamento(false)
-        setResumo(true)
+  const handleClickParcelamento = (e) => {
+      const parcela = e.target.value
+      console.log(parcela)
+  }
+  return (
 
-    }
-    return(
-        <>
-            <div className='form-pagamento'>
-                <form className='col-12 col-lg-8 '>
-                    <div className=' d-flex flex-column'>
-                        <h3 className='fw-bold'>detalhes do pagamento</h3>
-                        <div className='d-flex flex-column mt-3'>
-                            <label htmlFor="">Nome do titular</label>
-                            <input className='mt-1' type="text" required/>
-                        </div>
-
-                        <div className='d-flex flex-column mt-3'>
-                            <label htmlFor="">Número do cartão</label>
-                            <input className='mt-1' type="number" required
-                            />
-                        </div>
-                        <div className=' d-flex flex-row mt-3 vencimento-cvv'>
-                            <div className='d-flex flex-column mt-3'>
-                                <label htmlFor="">Vencimento</label>
-                                <input className='mt-1' type="text" placeholder='MM/AA'  required/>
-                            </div>
-
-                            <div className='d-flex flex-column mt-3 CVV'>
-                                <label htmlFor="">CVV</label>
-                                <input className='mt-1 '  type="number"  required/>
-                            </div>
-                        </div>
-                        <div className='mt-3'>
-                            <b>Clique aqui</b> para opções de parcelamento
-                        </div>
-                        <button onClick={handleClick} className='mt-3 botao' >Continuar</button>
-                    </div>
-                </form>
+    <>
+      <div className="form-pagamento">
+        <form className="col-12 col-lg-8 ">
+          <div className=" d-flex flex-column">
+            <h3 className="fw-bold">detalhes do pagamento</h3>
+            <div className="d-flex flex-column mt-3">
+              <label htmlFor="">Nome do titular</label>
+              <input className="mt-1" type="text" required />
             </div>
-        </>
-    )
+
+            <div className="d-flex flex-column mt-3">
+              <label htmlFor="">Número do cartão</label>
+              <input className="mt-1" type="number" required />
+            </div>
+            <div className=" d-flex flex-row mt-3 vencimento-cvv">
+              <div className="d-flex flex-column mt-3">
+                <label htmlFor="">Vencimento</label>
+                <input
+                  className="mt-1"
+                  type="text"
+                  placeholder="MM/AA"
+                  required
+                />
+              </div>
+
+              <div className="d-flex flex-column mt-3 CVV">
+                <label htmlFor="">CVV</label>
+                <input className="mt-1" type="number" required />
+              </div>
+            </div>
+            <div className="mt-3 d-flex flex-column">
+                <p>opções de parcelamento</p>
+              <select 
+              className="form-select" 
+              aria-label="Default select example" 
+              onClick={handleClickParcelamento}
+              >
+                <option value="1" selected>Pagamento à vista</option>
+                <option value="2">2x sem juros</option>
+                <option value="3">3x sem juros</option>
+                <option value="4">4x sem juros</option>
+              </select>
+            </div>
+            <button onClick={handleClick} className="mt-3 botao">
+              Continuar
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }

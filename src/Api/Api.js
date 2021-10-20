defea22ce30ef6c05359f7921449bca97c03bef3
vitorlombardi <83724421+vitorlombardi@users.noreply.github.com>
2021-10-20 18:16:18 -0300

    update api (create rota de update)

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -9,6 +9,8 @@ export const Api = {
     //url post
     creatClientUrl: () => Api.baseURL + "/Client",
 
+    //url path
+    updateClienteUrl: (id) => Api.baseURL + `/Client/${id}`,
 
     //url Auth
     registerAuthUrl: () => Api.baseURL + "/Auth/Register",
@@ -34,6 +36,15 @@ export const Api = {
     buildApiPostRequest: (url, body) =>
     fetch(url, {
       method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(body),
+    }),
+
+    buildApiPathRequest: (url, body) =>
+    fetch(url, {
+      method: "PATH",
       headers: new Headers({
         "Content-Type": "application/json",
       }),
